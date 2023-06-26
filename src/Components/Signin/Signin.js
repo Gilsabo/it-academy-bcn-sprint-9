6b9114f6d127a1afd1cce08b11533e0e4102b294
Gil Sala <gilsala_88@hotmail.com>
2023-06-26 11:34:04 +0200

    Add link to signin and signup

--- src/Components/Signin/Signin.js
+++ src/Components/Signin/Signin.js
@@ -1,6 +1,17 @@
+import { Link } from "react-router-dom";
 const Signin = () => {
     return ( 
-        <div>Signin</div>
+        <>
+        <h3>Sign in to your account</h3>
+        <p>Don't have an account? <Link to="/signup">Sign up.</Link></p>
+        <form>
+            <label htmlFor="user">Email adress</label>
+            <input type="email" />
+            <label htmlFor="password">Password</label>
+            <input type="password" />
+            <button>Sign in</button>
+        </form>
+        </>
      );
 }
  
--- src/Components/Signup/Signup.js
+++ src/Components/Signup/Signup.js
@@ -1,6 +1,17 @@
+import { Link } from "react-router-dom";
 const Signup = () => {
     return ( 
-        <div>Signup</div>
+        <>
+        <h3>Sign up for a free account</h3>
+        <p>Already have an account? <Link to="/signin">Sign up.</Link></p>
+        <form>
+            <label htmlFor="user">Email adress</label>
+            <input type="email" />
+            <label htmlFor="password">Password</label>
+            <input type="password" />
+            <button>Sign in</button>
+        </form>
+        </>
      );
 }
  
