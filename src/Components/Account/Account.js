48abcc400e4cd6cf4357a54e8dc13810005a01fd
Gil Sala <gilsala_88@hotmail.com>
2023-06-28 09:32:31 +0200

    Add conditional rendering if there is no user

--- src/Components/Account/Account.js
+++ src/Components/Account/Account.js
@@ -1,22 +1,28 @@
 import { useNavigate } from "react-router-dom";
 import { UserAuth } from "../../context/Auth-context";
 const Account = () => {
-    const {user,logout} = UserAuth()
-    const navigate =useNavigate()
-    const handleLogOut = async () =>{
-        try{
-        await logout()
-        navigate('/signin')
-        }catch(e){
-            console.log(e.message)
-        }
+  const { user, logout } = UserAuth();
+  const navigate = useNavigate();
+  const handleLogOut = async () => {
+    try {
+      await logout();
+      navigate("/signin");
+    } catch (e) {
+      console.log(e.message);
     }
-    return ( 
+  };
+  return (
+    <>
+      {user ? (
         <>
-        <div>User Email:{user && user.email}</div>
-        <button onClick={handleLogOut}>Logout</button>
+          <div>User Email:{user && user.email}</div>
+          <button onClick={handleLogOut}>Logout</button>
         </>
-     );
-}
- 
+      ) : (
+        ""
+      )}
+    </>
+  );
+};
+
 export default Account;
