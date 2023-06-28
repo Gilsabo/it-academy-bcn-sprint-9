7d97f74d2f576c439d3e12b079d19d3a8233334c
Gil Sala <gilsala_88@hotmail.com>
2023-06-28 09:08:48 +0200

    Add protected routes

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import Signin from "./Components/Signin/Signin.js";
 import Signup from "./Components/Signup/Signup.js";
 import { AuthContextProvider } from "./context/Auth-context";
 import Account from "./Components/Account/Account";
+import ProtectedRoute from "./Components/ProtectedRoute/ProtectedRoute";
 
 function App() {
   return (
@@ -26,14 +27,14 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/mens" element={<Mens />} />
-            <Route path="/mens/:id" element={<Product />} />
+            <Route path="/mens/:id" element={<ProtectedRoute><Product /></ProtectedRoute >} />
             <Route path="/women" element={<Women />} />
-            <Route path="/women/:id" element={<Product />} />
+            <Route path="/women/:id" element={<ProtectedRoute><Product /></ProtectedRoute>} />
             <Route path="/electronics" element={<Electronics />} />
-            <Route path="/electronics/:id" element={<Product />} />
+            <Route path="/electronics/:id" element={<ProtectedRoute><Product /></ProtectedRoute>} />
             <Route path="/jewelery" element={<Jewelery />} />
-            <Route path="/jewelery/:id" element={<Product />} />
-            <Route path="/cart" element={<ShoppingCart />} />
+            <Route path="/jewelery/:id" element={<ProtectedRoute><Product /></ProtectedRoute>} />
+            <Route path="/cart" element={<ProtectedRoute><ShoppingCart /></ProtectedRoute>} />
             <Route path="/signin" element={<Signin />} />
             <Route path="/signup" element={<Signup />} />
             <Route path="/account" element={<Account />} />
