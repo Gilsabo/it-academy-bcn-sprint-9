7f3be2d1267b4074201123b29168676f856d37ec
Gil Sala <gilsala_88@hotmail.com>
2023-06-28 09:09:10 +0200

    Set protected routes

---
+++ src/Components/ProtectedRoute/ProtectedRoute.js
@@ -0,0 +1,16 @@
+import { Navigate } from "react-router-dom";
+import { UserAuth } from "../../context/Auth-context";
+
+
+const ProtectedRoute = ({children}) => {
+    
+    const {user} = UserAuth()
+    if(!user) {
+        return <Navigate to= "/signin"/>
+    }
+    return ( 
+        children
+     );
+}
+ 
+export default ProtectedRoute;
