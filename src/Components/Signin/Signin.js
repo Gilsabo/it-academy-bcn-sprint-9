b2622a4e26b5f91329dcffad8e9fa5004c321576
Gil Sala <gilsala_88@hotmail.com>
2023-07-04 11:44:56 +0200

    Add styles to sign in and sign up

--- src/Components/Signin/Signin.js
+++ src/Components/Signin/Signin.js
@@ -1,40 +1,52 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { UserAuth } from "../../context/Auth-context";
+import youtubeLogo from "../../images/youtube-logo.png";
+import "./Signin.css";
 const Signin = () => {
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const [error, setError] = useState('')
-    const { signIn} =UserAuth()
-    const navigate = useNavigate()
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const { signIn } = UserAuth();
+  const navigate = useNavigate();
 
-    const handleSubmit = async(e)=>{
-        e.preventDefault()
-        setError('')
-        navigate('/')
-        try{
-            await signIn(email, password)
-
-        }catch(e){
-            setError(e.message)
-            console.log(e.message)
-
-        }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError("");
+    navigate("/");
+    try {
+      await signIn(email, password);
+    } catch (e) {
+      setError(e.message);
+      console.log(e.message);
     }
+  };
 
-    return ( 
-        <>
+  return (
+    <main className="main-box">
+      <div className="left-box">
+        <h2>Sign in to keep purchasing </h2>
+        <h2>Sign in to grow your audience </h2>
+        <img src={youtubeLogo} alt="youtube-logo" className="youtube-logo" />
+      </div>
+      <div className="right-box">
         <h3>Sign in to your account</h3>
-        <p>Don't have an account? <Link to="/signup">Sign up.</Link></p>
+        <p>
+          Don't have an account? <Link to="/signup">Sign up.</Link>
+        </p>
         <form onSubmit={handleSubmit}>
-            <label htmlFor="user">Email adress</label>
-            <input onChange={(e)=>setEmail(e.target.value)}type="email" />
-            <label htmlFor="password">Password</label>
-            <input type="password" onChange={(e)=>setPassword(e.target.value)}/>
-            <button>Sign in</button>
+          <label htmlFor="user">Email adress</label>
+          <input onChange={(e) => setEmail(e.target.value)} type="email" />
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button className="sign-in-button">Sign in</button>
         </form>
-        </>
-     );
-}
- 
+      </div>
+    </main>
+  );
+};
+
 export default Signin;
--- src/Components/Signup/Signup.js
+++ src/Components/Signup/Signup.js
@@ -1,39 +1,50 @@
-import { Link, useNavigate  } from "react-router-dom";
-
+import { Link, useNavigate } from "react-router-dom";
+import "./Signup.css"
 import { useState } from "react";
-import { UserAuth } from "../../context/Auth-context"
+import { UserAuth } from "../../context/Auth-context";
+import youtubeLogo from "../../images/youtube-logo.png"
 const Signup = () => {
-
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const [error, setError] = useState('')
-    const {createUser} = UserAuth()
-    const navigate = useNavigate()
-    const handleSubmit = async(e)=>{
-        e.preventDefault()
-        setError('')
-        try{
-            await createUser(email, password)
-            navigate('/')
-        }catch(e){
-            setError(e.message)
-            console.log(e.message)
-
-        }
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const { createUser } = UserAuth();
+  const navigate = useNavigate();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError("");
+    try {
+      await createUser(email, password);
+      navigate("/");
+    } catch (e) {
+      setError(e.message);
+      console.log(e.message);
     }
-    return ( 
-        <>
+  };
+  return (
+    <main className="main-box">
+      <div className="left-box">
+        <h2>Sign up and start your dream</h2>
+        <h2>"Likes" are awaiting you</h2>
+        <img src={youtubeLogo} alt="youtube-logo" className="youtube-logo"/>
+      </div>
+      <div className="right-box">
         <h3>Sign up for a free account</h3>
-        <p>Already have an account? <Link to="/signin">Sign in.</Link></p>
+        <p>
+          Already have an account? <Link to="/signin">Sign in.</Link>
+        </p>
         <form onSubmit={handleSubmit}>
-            <label htmlFor="user">Email adress</label>
-            <input type="email" onChange={(e)=>setEmail(e.target.value)}/>
-            <label htmlFor="password">Password</label>
-            <input type="password" onChange={(e)=>setPassword(e.target.value)}/>
-            <button>Sign up</button>
+          <label htmlFor="user">Email adress</label>
+          <input type="email" onChange={(e) => setEmail(e.target.value)} />
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button className="sign-up-button">Sign up</button>
         </form>
-        </>
-     );
-}
- 
+      </div>
+    </main>
+  );
+};
+
 export default Signup;
