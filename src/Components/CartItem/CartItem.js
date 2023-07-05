5d23df0aaa888dfdd13711e8762170bd8ca8fba4
Gil Sala <gilsala_88@hotmail.com>
2023-07-05 20:20:04 +0200

    Add styles shopping cart

--- src/Components/CartItem/CartItem.js
+++ src/Components/CartItem/CartItem.js
@@ -1,22 +1,36 @@
 import { useContext } from "react";
 import { ShopContext } from "../../context/shop-context";
-import "./CartItem.css"
+import "./CartItem.css";
 
 const CartItem = ({ item }) => {
-  
-  const { cartItem, addToCart, removeFromCart, updateCartItemCount } = useContext(ShopContext);
+  const { cartItem, addToCart, removeFromCart, updateCartItemCount } =
+    useContext(ShopContext);
   console.log(cartItem);
 
   return (
     <>
       <div className="item">
-        <img src={item.image} alt="product" style={{ width: "200px" }} />
+        <img src={item.image} alt="product" className="image-cart" />
         <div>{item.title}</div>
-      </div>
-      <div className="countHandler">
-        <button className="button-cart" onClick={()=>removeFromCart(item.id)}>-</button>
-        <input className="input-quantity"value={cartItem[item.id]} onChange={(e)=>updateCartItemCount(Number(e.target.value), item.id)}/>
-        <button className="button-cart" onClick={()=>addToCart(item.id)}>+</button>
+
+        <div className="countHandler">
+          <button
+            className="button-cart"
+            onClick={() => removeFromCart(item.id)}
+          >
+            -
+          </button>
+          <input
+            className="input-quantity"
+            value={cartItem[item.id]}
+            onChange={(e) =>
+              updateCartItemCount(Number(e.target.value), item.id)
+            }
+          />
+          <button className="button-cart" onClick={() => addToCart(item.id)}>
+            +
+          </button>
+        </div>
       </div>
     </>
   );
--- src/Pages/ShoppingCart.js
+++ src/Pages/ShoppingCart.js
@@ -3,10 +3,12 @@ import { useEffect, useState, useContext } from "react";
 import { ShopContext } from "../context/shop-context";
 import CartItem from "../Components/CartItem/CartItem";
 import { Link } from "react-router-dom";
+import "./ShoppingCart.css"
+
 const ShoppingCart = () => {
   const [fetchData, setFetchData] = useState([]);
   const { cartItem, getTotalCartAmount  } = useContext(ShopContext);
-  const totalAmount = getTotalCartAmount()
+  const totalAmount = getTotalCartAmount().toFixed(2)
 
   useEffect(() => {
     axios
@@ -25,20 +27,20 @@ const ShoppingCart = () => {
 console.log(totalAmount)
 
   return (
-    <>
-      <h1>Shopping cart</h1>
-      <div className="item">
+    <div className="shopping-cart-container">
+      <h1 className="header-cart">Shopping cart</h1>
+      <div className="items">
         {fetchData.map((item) => {
           if (cartItem[item.id] !== 0) return <CartItem item={item} />;
         })}
       </div>
       <div className="checkout">
-        <p>subtotal: {totalAmount} $</p>
-        <Link to="/"><button> Continue Shopping</button></Link>
+        <p>subtotal: {totalAmount} euros</p>
+        <Link to="/" className="link-shopping"><button className="button-shopping"> Continue Shopping</button></Link>
         
-        <button>Checkout</button>
+        <button className="button-shopping">Checkout</button>
       </div>
-    </>
+    </div>
   );
 };
 
