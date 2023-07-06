6bd5938dd587656a3655da2c3cc87239910a1b48
Gil Sala <gilsala_88@hotmail.com>
2023-07-06 09:01:13 +0200

    Add responsivness to Navbar

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -3,71 +3,74 @@ import { ShoppingCart } from "phosphor-react";
 import Account from "../Account/Account";
 import "./Navbar.css";
 import { UserAuth } from "../../context/Auth-context";
+
 const Navbar = () => {
   const { user } = UserAuth();
 
   return (
     <nav className="navbar">
-      <ul className="nav-left-side">
-        <li className="list-box">
-          <Link className="link-nav" to="/">
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link className="link-nav" to="/mens">
-            Men's clothing
-          </Link>
-        </li>
-        <li>
-          <Link className="link-nav" to="/women">
-            Women's clothing
-          </Link>
-        </li>
-        <li>
-          <Link className="link-nav" to="/electronics">
-            Electronics
-          </Link>
-        </li>
-        <li>
-          <Link className="link-nav" to="/jewelery">
-            Jewelery
-          </Link>
-        </li>
-        <li>
-          <Link className="link-nav" to="/cart">
-            <ShoppingCart />
-          </Link>
-        </li>
-      </ul>
-      <ul className="nav-right-side">
-        {!user ? (
-          <div className="links">
-            <li>
-              <Link className="link-nav" to="/signin">
-                Sign in
-              </Link>
-            </li>
-            <li>
-              <Link className="link-nav" to="/signup">
-                Sign up
-              </Link>
-            </li>
-          </div>
-        ) : (
-          ""
-        )}
+      <div className="navbar-container">
+        <ul className="nav-left-side">
+          <li className="list-box">
+            <Link className="link-nav" to="/">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link className="link-nav" to="/mens">
+              Men's clothing
+            </Link>
+          </li>
+          <li>
+            <Link className="link-nav" to="/women">
+              Women's clothing
+            </Link>
+          </li>
+          <li>
+            <Link className="link-nav" to="/electronics">
+              Electronics
+            </Link>
+          </li>
+          <li>
+            <Link className="link-nav" to="/jewelery">
+              Jewelery
+            </Link>
+          </li>
+          <li>
+            <Link className="link-nav" to="/cart">
+              <ShoppingCart />
+            </Link>
+          </li>
+        </ul>
+        <ul className="nav-right-side">
+          {!user ? (
+            <div className="links">
+              <li className="link-sign-in">
+                <Link className="link-nav" to="/signin">
+                  Sign in
+                </Link>
+              </li>
+              <li className="link-sign-up">
+                <Link className="link-nav" to="/signup">
+                  Sign up
+                </Link>
+              </li>
+            </div>
+          ) : (
+            ""
+          )}
 
-        {user ? (
-          <div className="account">
-            <li className="link-nav account">
-              <Account />
-            </li>
-          </div>
-        ) : (
-          ""
-        )}
-      </ul>
+          {user ? (
+            <div className="account">
+              <li className="link-nav account">
+                <Account />
+              </li>
+            </div>
+          ) : (
+            ""
+          )}
+        </ul>
+      </div>
     </nav>
   );
 };
